757541f1e1afd3a2525953b417e01b5cfeb02ba8
پک¤پK“©”پ <spamies@live.com>
2019-09-16 14:08:47 +0100

    Update discord.js

--- discord.js
+++ discord.js
@@ -1,4 +1,12 @@
 (async function () {
   let script = await fetch('owoify.js')
-  console.log(script)
+  let text = await script.text()
+  let output = [
+    '(function () {',
+    ...text.split('\n').map(line => `  ${line}`),
+    '})()'
+  ].join('\n')
+  let pre = document.createElement('pre')
+  pre.textContent = output
+  document.body.appendChild(pre)
 })()
