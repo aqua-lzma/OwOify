49661d4f7709dff0961aa3c7da50ca40103aeb06
پک¤پK“©”پ <spamies@live.com>
2019-09-16 13:49:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,81 @@
+let owo
+
 function rebuildWordMap () {
-  
+  let wordMap = document.querySelector('#wordMap')
+  let remove = []
+  let results = {}
+  for (let row of wordMap.children) {
+    let from = row.children[0].firstChild.value
+    let to = row.children[1].firstChild.value
+    if (from !== '' && to !== '') {
+      results[from] = to
+    } else if (from === '' && to === '') {
+      remove.push(row)
+    }
+  }
+  owo.wordMap = results
+  reTranslate()
+  for (let row of remove) {
+    wordMap.removeChild(row)
+  }
+  let row = document.createElement('tr')
+  let td1 = document.createElement('td')
+  let td2 = document.createElement('td')
+  let input1 = document.createElement('input')
+  let input2 = document.createElement('input')
+  input1.style.width = input2.style.width = '100%'
+  input1.addEventListener('keyup', rebuildWordMap)
+  input2.addEventListener('keyup', rebuildWordMap)
+  td1.appendChild(input1)
+  td2.appendChild(input2)
+  row.appendChild(td1)
+  row.appendChild(td2)
+  wordMap.appendChild(row)
 }
 
 function rebuildPrefixesSuffixes () {
-  
+  let prefixesSuffixes = document.querySelector('#prefixesSuffixes')
+  let remove = []
+  let prefixes = []
+  let suffixes = []
+  for (let row of prefixesSuffixes.children) {
+    let prefix = row.children[0].firstChild.value
+    let suffix = row.children[1].firstChild.value
+    if (prefix === '' && suffix === '') {
+      remove.push(row)
+    } else {
+      if (prefix !== '') prefixes.push(prefix)
+      if (suffix !== '') suffixes.push(prefix)
+    }
+  }
+  owo.prefixes = prefixes
+  owo.suffixes = suffixes
+  reTranslate()
+  for (let row of remove) {
+    prefixesSuffixes.removeChild(row)
+  }
+  let row = document.createElement('tr')
+  let td1 = document.createElement('td')
+  let td2 = document.createElement('td')
+  let input1 = document.createElement('input')
+  let input2 = document.createElement('input')
+  input1.style.width = input2.style.width = '100%'
+  input1.addEventListener('keyup', rebuildPrefixesSuffixes)
+  input2.addEventListener('keyup', rebuildPrefixesSuffixes)
+  td1.appendChild(input1)
+  td2.appendChild(input2)
+  row.appendChild(td1)
+  row.appendChild(td2)
+  prefixesSuffixes.appendChild(row)
+}
+
+function reTranslate () {
+  document.querySelector('#output').value = owo.owoify(document.querySelector('#input').value)
 }
 
 window.addEventListener('load', () => {
-  let owo = new OwO()
-  document.querySelector('#input').addEventListener('keyup', function () {
-    document.querySelector('#output').value = owo.owoify(this.value)
-  })
+  owo = new OwO()
+  document.querySelector('#input').addEventListener('keyup', () => reTranslate())
   // --- Build main options ---
   let options = [
     ['rltow', 'R and L to W'],
@@ -30,6 +95,7 @@ window.addEventListener('load', () => {
     input.checked = true
     input.addEventListener('change', () => {
       owo[key] = input.value
+      reTranslate()
     })
     td2.textContent = description
     td1.appendChild(input)
@@ -51,6 +117,7 @@ window.addEventListener('load', () => {
     input.value = defaultValue
     input.addEventListener('keyup', () => {
       owo[key] = Number(input.value)
+      reTranslate()
     })
     td2.textContent = description
     td1.appendChild(input)
@@ -77,5 +144,25 @@ window.addEventListener('load', () => {
     row.appendChild(td2)
     wordMap.appendChild(row)
   }
+  rebuildWordMap()
   // --- Build prefixes & suffixes ---
+  let prefixesSuffixes = document.querySelector('#prefixesSuffixes')
+  for (let i = 0; i < owo.prefixes.length || i < owo.suffixes.length; i++) {
+    let row = document.createElement('tr')
+    let td1 = document.createElement('td')
+    let td2 = document.createElement('td')
+    let input1 = document.createElement('input')
+    let input2 = document.createElement('input')
+    input1.style.width = input2.style.width = '100%'
+    input1.value = owo.prefixes[i] != null ? owo.prefixes[i] : ''
+    input2.value = owo.suffixes[i] != null ? owo.suffixes[i] : ''
+    input1.addEventListener('keyup', rebuildPrefixesSuffixes)
+    input2.addEventListener('keyup', rebuildPrefixesSuffixes)
+    td1.appendChild(input1)
+    td2.appendChild(input2)
+    row.appendChild(td1)
+    row.appendChild(td2)
+    prefixesSuffixes.appendChild(row)
+  }
+  rebuildPrefixesSuffixes()
 })
